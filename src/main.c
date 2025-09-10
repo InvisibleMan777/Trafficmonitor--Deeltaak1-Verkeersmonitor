4d7f9d6f285a0e5d1d1512c43c7e863ff58d62af
Sem Overbeek <s1212918@student.windesheim.nl>
2025-09-10 22:21:25 +0200

    update datatypes

--- src/main.c
+++ src/main.c
@@ -4,16 +4,16 @@
 #include <stdio.h>
 #include <util/delay.h>
 
-#define ANTI_DEBOUNCE_DELAY 10
+#define ANTI_DEBOUNCE_DELAY 10 //time in ms between checks if buttons are released to prevent the negative implications of debounce
 
 int main() {
-    int unitCounter = 0; //counter of units that have passed triggers of button0 and button1
+    unsigned int unitCounter = 0; //counter of units that have passed button0 and button1
     bool buttonPressed[2] = {false, false}; //button states of button0 and button1
-    int unitsInProcess = 0; //counter of units between triggers of button0 and button1
+    unsigned short int unitsInProcess = 0; //counter of units that have passed button0 but not yet button1
 
-    //pin 8-11 are outputs
+    //pb0-pb3 (pin 8-11 on the arduino) are outputs
     DDRB |= (1 << DDB0 | 1 << DDB1 | 1 << DDB2 | 1 << DDB3); 
-    //pin 2 and 3 are pullup inputs
+    //pd2 and pd3 (pin 2 and 3 on the arduino) are pullup inputs
     PORTD |= (1 << PORTD2 | 1 << PORTD3);
 
     //initialize serial communication
@@ -38,7 +38,10 @@ int main() {
             case false:
                 //button is being pressed
                 if (!(PIND & (1 << PIND2))) {
-                    unitsInProcess++;
+                    //not more then 255 units in process
+                    if (unitsInProcess < 255) {
+                        unitsInProcess++;
+                    }
                     buttonPressed[0] = true;
                 }
                 break;
@@ -58,14 +61,13 @@ int main() {
             case false:
                 //button is being pressed
                 if (!(PIND & (1 << PIND3))) {
-                    //if there is at least 1 trigger in process (initialized by button0)
                     if (unitsInProcess > 0) {
                         unitCounter++;
                         unitsInProcess--;
 
                         //cast to string is neccesairy to transmit the count
                         char str[2400];
-                        sprintf(str, "%d", unitCounter);
+                        sprintf(str, "%u", unitCounter);
 
                         //transmit unit count to serial
                         for (int i = 0; str[i] != '\0'; i++) {
