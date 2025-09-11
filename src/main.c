af750eb6b573107e49a09d0d1f86db47d4ec2fc0
Sem Overbeek <s1212918@student.windesheim.nl>
2025-09-11 09:56:32 +0200

    upgraded to non-blocking code

--- src/main.c
+++ src/main.c
@@ -3,31 +3,47 @@
 #include <usart.h>
 #include <stdio.h>
 #include <util/delay.h>
+#include <Arduino.h>
 
-#define ANTI_DEBOUNCE_DELAY 10 //time in ms between checks if buttons are released to prevent the negative implications of debounce
+#define ANTI_DEBOUNCE_DELAY 200 //time in ms between checks if buttons are released to prevent the negative implications of debounce
+
+//initialize IO pins
+void initialiseIO() {
+    //pb0-pb3 (pin 8-11 on the arduino) are outputs
+    DDRB |= (1 << DDB0 | 1 << DDB1 | 1 << DDB2 | 1 << DDB3); 
+    //pd2 and pd3 (pin 2 and 3 on the arduino) are pullup inputs
+    PORTD |= (1 << PORTD2 | 1 << PORTD3);
+}
+
+//updating the leds (pb0-pb3) to display the unit count in binary every cycle
+void displayCounter(int unitCounter) {
+    PORTB ^= PORTB ^ (unitCounter & (1 << PORTB0 | 1 << PORTB1 | 1 << PORTB2 | 1 << PORTB3));
+}
 
 int main() {
     unsigned int unitCounter = 0; //counter of units that have passed button0 and button1
     bool buttonPressed[2] = {false, false}; //button states of button0 and button1
     unsigned short int unitsInProcess = 0; //counter of units that have passed button0 but not yet button1
+    unsigned long timeSinceLastTrigger = millis(); //time since last button press
 
-    //pb0-pb3 (pin 8-11 on the arduino) are outputs
-    DDRB |= (1 << DDB0 | 1 << DDB1 | 1 << DDB2 | 1 << DDB3); 
-    //pd2 and pd3 (pin 2 and 3 on the arduino) are pullup inputs
-    PORTD |= (1 << PORTD2 | 1 << PORTD3);
-
+    //initialise arduino
+    init();
+    //initialize io pins
+    initialiseIO();
     //initialize serial communication
     USART_Init();
 
     //main loop
     while (true) {
         //updating the leds (pb0-pb3) to display the unit count in binary every cycle
-        PORTB ^= PORTB ^ (unitCounter & (1 << PORTB0 | 1 << PORTB1 | 1 << PORTB2 | 1 << PORTB3));
+        displayCounter(unitCounter);
 
         //check state of button0
         switch (buttonPressed[0]) {
             case true:
-                _delay_ms(ANTI_DEBOUNCE_DELAY);
+                if (millis() - timeSinceLastTrigger < ANTI_DEBOUNCE_DELAY) {
+                    break;
+                }
 
                 //button is being released
                 if (PIND & (1 << PIND2)) {
@@ -41,8 +57,10 @@ int main() {
                     //not more then 255 units in process
                     if (unitsInProcess < 255) {
                         unitsInProcess++;
-                    }
+                    }       
+
                     buttonPressed[0] = true;
+                    timeSinceLastTrigger = millis();
                 }
                 break;
         }
@@ -50,7 +68,9 @@ int main() {
         //check state of button1
         switch (buttonPressed[1]) {
             case true:
-                _delay_ms(ANTI_DEBOUNCE_DELAY);
+                if (millis() - timeSinceLastTrigger < ANTI_DEBOUNCE_DELAY) {
+                    break;
+                }
 
                 //button is being released
                 if (PIND & (1 << PIND3)) {
@@ -76,6 +96,7 @@ int main() {
                         USART_Transmit('\n');
 
                         buttonPressed[1] = true;
+                        timeSinceLastTrigger = millis();
                     }
                 }
                 break;
